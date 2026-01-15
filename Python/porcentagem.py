6e0fd23f6cf572af3520f96138b7df85647d1c5e
DaviDiasMota <ddmota2009@gmail.com>
2026-01-15 17:26:10 -0300

    Delete Python/porcentagem.py

--- Python/porcentagem.py
+++
@@ -1,8 +0,0 @@
-
-print("Calculadora de Porcentagem")
-
-total = float(input("Digite aqui o valor total: "))
-parte = float(input("Digite aqui o valor da porcentagem do to que você quer calcular: "))
-porcentagem = total *(parte/100)
-
-print(parte,"% de", total, "é:", porcentagem)
