f1fb801609405fb9a619bd9ccbe08be632295e65
DaviDiasMota <ddmota2009@gmail.com>
2026-01-19 14:05:12 -0300

    Delete Python/media.py

--- Python/media.py
+++
@@ -1,6 +0,0 @@
-n1 = float(input("Digite a primeira nota do aluno:"))
-n2 = float(input("Digite aqui a segunda nota do aluno: "))
-n3 = float(input("Digite aqui a terceira nota do aluno: "))
-media = (n1 + n2 + n3)/3
-
-print("A média do aluno é: ", media)
