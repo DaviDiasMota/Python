e44309ef8aa5abcd5682d67b4548bfcea7f29e5b
DaviDiasMota <ddmota2009@gmail.com>
2026-01-19 14:05:50 -0300

    Delete Python/calculadora_de_autonomia.py

--- Python/calculadora_de_autonomia.py
+++
@@ -1,5 +0,0 @@
-litros = float(input("Digite aqui quantos litros você tem disponivel:"))
-gasto = float(input("Quantos quilometros o seu veiculo faz por litro?"))
-distancia = litros*gasto
-
-print("O seu carro, percorrirá", distancia, "Km")
